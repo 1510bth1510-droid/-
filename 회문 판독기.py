0f0b3b0201e395ef24266e10228ef6854abed812
1510bth1510-droid <1510bth1510@gmail.com>
2026-03-11 18:40:09 +0900

    Add files via upload

--- 회문 판독기.py
+++ 회문 판독기.py
@@ -4,7 +4,10 @@ while True:
     a[::-1]
     if len(a) == 1: 
         print("한 글자 입니다.")
+        break
     elif a == a[::-1]:
         print("회문입니다.")
+        break
     else:
         print("회문이 아닙니다.")
+        break
