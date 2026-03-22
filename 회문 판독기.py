39866efecb1e30c7291c1b39240be977e6b5a699
353535bu <1510bth1510@gmail.com>
2026-03-22 21:01:53 +0900

    Add files via upload

--- 회문 판독기.py
+++ 회문 판독기.py
@@ -1,13 +1,34 @@
 while True:
+    print("="*40)
     a = (input("입력=>"))
     print("="*40)
-    a[::-1]
     if len(a) == 1: 
         print("한 글자 입니다.")
-        break
+        print("="*40)
+        b = int(input('다시 시작 = 1번\n종료 = 2번'))
+        if b == 1:
+            continue
+        elif b == 2:
+            break
+        else:
+            print('잘못된 입력')
     elif a == a[::-1]:
         print("회문입니다.")
-        break
+        print("="*40)
+        b = int(input('다시 시작 = 1번\n종료 = 2번'))
+        if b == 1:
+            continue
+        elif b == 2:
+            break
+        else:
+            print('잘못된 입력')
     else:
         print("회문이 아닙니다.")
-        break
+        print("="*40)
+        b = int(input('다시 시작 = 1번\n종료 = 2번'))
+        if b == 1:
+            continue
+        elif b == 2:
+            break
+        else:
+            print('잘못된 입력')
